cee3d8adc4754ccf8e00a02aa884ed42bf1f6eea
senthanal <senthanal@gmail.com>
2025-01-03 15:56:15 +0100

    first successful ort scan from docker

--- src/ort-scan.ts
+++ src/ort-scan.ts
@@ -2,19 +2,22 @@ import { Store } from "./Store";
 import { existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from "fs";
 import { join, resolve } from "path";
 import { PackageJson } from "type-fest";
-import Docker, { ContainerInfo } from "dockerode";
-import { getChunks } from "./utils";
+import { checkCmdError, cmd } from "./utils";
 
 export class OrtScan {
   private readonly containerName = "deep-scan";
   private readonly packagePath = resolve(__dirname, "../", "project-scan");
   private readonly templatePath = resolve(__dirname, "templates");
-  private readonly dockerInstance: Docker;
 
-  public constructor() {
-    this.dockerInstance = new Docker({ host: "localhost", port: 2375 });
-  }
-
+  /**
+   * Scans the given package and version with the ORT tool. This will create a
+   * temporary directory with the given package and version as a dependency, and
+   * then build a Docker image from it. The Docker image will then be run in a
+   * container which will be stopped and removed after it has finished running.
+   *
+   * @param packageName - The name of the package to scan.
+   * @param packageVersion - The version of the package to scan.
+   */
   public async scan(
     packageName: string,
     packageVersion: string
@@ -24,13 +27,21 @@ export class OrtScan {
     this.writePackageJson(packageJson);
     this.copyDockerfile();
     this.copyDockerEntry();
-    await this.removeDockerContainer();
-    await this.buildDockerImage();
-    await this.runDockerContainer();
-    await this.stopDockerContainer();
-    await this.removeDockerContainer();
+    this.buildDockerImage();
+    this.stopDockerContainer();
+    this.removeDockerContainer();
+    this.createDockerContainer();
+    this.startDockerContainer();
+    this.stopDockerContainer();
+    this.removeDockerContainer();
   }
 
+  /**
+   * Creates the directory for the scan project if it does not already exist, or
+   * cleans it if it does exist. This is necessary because the directory is used
+   * to store the temporary files for the scan, and we don't want to leave any
+   * files behind after the scan has finished running.
+   */
   private createPackagePath(): void {
     if (existsSync(this.packagePath)) {
       Store.getInstance().addMessage(`Scan project directory already exists`);
@@ -43,6 +54,16 @@ export class OrtScan {
     Store.getInstance().addMessage(`done`);
   }
 
+  /**
+   * Reads the package.json file from the template directory, adds the given
+   * package and version to the dependencies, and returns the package.json
+   * object.
+   *
+   * @param packageName - The name of the package to add to the dependencies.
+   * @param packageVersion - The version of the package to add to the
+   *                         dependencies.
+   * @returns The package.json object with the added package and version.
+   */
   private getPackageJson(
     packageName: string,
     packageVersion: string
@@ -60,6 +81,10 @@ export class OrtScan {
     return packageJson;
   }
 
+  /**
+   * Writes the given package json to the scan project directory.
+   * @param packageJson - The package json to write.
+   */
   private writePackageJson(packageJson: PackageJson): void {
     Store.getInstance().addMessage(`Writing package json to the scan project`);
     const content = JSON.stringify(packageJson, null, 2);
@@ -68,6 +93,12 @@ export class OrtScan {
     Store.getInstance().addMessage(`done`);
   }
 
+  /**
+   * Copies the Dockerfile from the template directory to the scan project
+   * directory. This is necessary because the Dockerfile is used to build the
+   * Docker image, and the Dockerfile must be in the same directory as the
+   * package.json file.
+   */
   private copyDockerfile(): void {
     Store.getInstance().addMessage(`Copying Dockerfile to the scan project`);
     const path = join(this.packagePath, "Dockerfile");
@@ -78,6 +109,12 @@ export class OrtScan {
     Store.getInstance().addMessage(`done`);
   }
 
+  /**
+   * Copies the entrypoint.sh file from the template directory to the scan project
+   * directory. This is necessary because the entrypoint.sh file is used to run the
+   * Docker container, and the entrypoint.sh file must be in the same directory as
+   * the package.json file.
+   */
   private copyDockerEntry(): void {
     Store.getInstance().addMessage(`Copying entrypoint.sh to the scan project`);
     const path = join(this.packagePath, "entrypoint.sh");
@@ -88,56 +125,85 @@ export class OrtScan {
     Store.getInstance().addMessage(`done`);
   }
 
-  private async buildDockerImage(): Promise<void> {
+  /**
+   * Builds a Docker image using the Dockerfile in the scan project directory.
+   * This image is tagged with the container's name. The build process does not
+   * use cache to ensure a fresh image is created. Any errors encountered during
+   * the build are logged to the store.
+   */
+  private buildDockerImage(): void {
     Store.getInstance().addMessage(`Building docker image`);
-    const stream = await this.dockerInstance.buildImage(
-      {
-        context: this.packagePath,
-        src: ["Dockerfile", "package.json", "entrypoint.sh"],
-      },
-      { t: this.containerName, rm: true, nocache: true }
-    );
-    const chunks = await getChunks(stream);
-    const output = chunks.toString("utf8");
-    Store.getInstance().addMessage(output, "verbose");
+    const buildImageCommand = `docker image build --no-cache --tag ${
+      this.containerName
+    } ${resolve(this.packagePath)}`;
+    const response = cmd(buildImageCommand);
+    checkCmdError(response, this.containerName);
     Store.getInstance().addMessage(`done`);
   }
 
-  private async runDockerContainer(): Promise<void> {
+  /**
+   * Creates a Docker container using the image created in {@link buildDockerImage}.
+   * The container is created with a volume mount to the results directory of the
+   * scan project, and the container is given the same name as the image. The
+   * container is not started automatically, but the container is stopped and
+   * removed after the scan is finished. Any errors encountered during the
+   * creation of the container are logged to the store.
+   */
+  private createDockerContainer(): void {
+    Store.getInstance().addMessage(`Creating docker container`);
+    const createContainerCommand = `docker container create --volume ${join(
+      this.packagePath,
+    )}:/home/ort/results:rw --name ${this.containerName} ${this.containerName}`;
+    const response = cmd(createContainerCommand);
+    checkCmdError(response, "");
+    Store.getInstance().addMessage(`done`);
+  }
+
+  /**
+   * Starts the Docker container created by {@link createDockerContainer}.
+   * The container is started in interactive mode. Any errors encountered
+   * during the start of the container are logged to the store.
+   */
+  private startDockerContainer(): void {
     Store.getInstance().addMessage(`Running docker container`);
-    await this.dockerInstance.run(this.containerName, [], [], {
-      name: this.containerName,
-      HostConfig: {
-        Binds: [
-          `${this.packagePath}/results:/home/ort/results:rw`,
-        ],
-      },
-    });
+    const startContainerCommand = `docker container start --interactive ${this.containerName}`;
+    const response = cmd(startContainerCommand);
+    checkCmdError(response, this.containerName);
     Store.getInstance().addMessage(`done`);
   }
 
-  private async stopDockerContainer(): Promise<void> {
+  private stopDockerContainer(): void {
     Store.getInstance().addMessage(`Stopping docker container`);
-    const containerInfo = await this.getContainerInfo(this.containerName);
-    if (containerInfo) {
-      const container = this.dockerInstance.getContainer(containerInfo.Id);
-      await container.stop();
+    const exists = this.existsDockerContainer();
+    if (exists) {
+      Store.getInstance().addMessage(`Docker container exists`);
+      const stopContainerCommand = `docker container stop ${this.containerName}`;
+      const response = cmd(stopContainerCommand);
+      checkCmdError(response, this.containerName);
+    }
+    else {
+      Store.getInstance().addMessage(`Docker container does not exist`);
     }
     Store.getInstance().addMessage(`done`);
   }
 
-  private async removeDockerContainer(): Promise<void> {
+  private removeDockerContainer(): void {
     Store.getInstance().addMessage(`Removing docker container`);
-    const containerInfo = await this.getContainerInfo(this.containerName);
-    if (containerInfo) {
-      const container = this.dockerInstance.getContainer(containerInfo.Id);
-      await container.remove();
+    const exists = this.existsDockerContainer();
+    if (exists) {
+      Store.getInstance().addMessage(`Docker container exists`);
+      const removeContainerCommand = `docker container rm --volumes ${this.containerName}`;
+      const response = cmd(removeContainerCommand);
+      checkCmdError(response, this.containerName);
+    } else {
+      Store.getInstance().addMessage(`Docker container does not exist`);
     }
     Store.getInstance().addMessage(`done`);
   }
 
-  private async getContainerInfo(name: string): Promise<ContainerInfo | undefined> {
-    const containers = await this.dockerInstance.listContainers();
-    return containers.find((container) => container.Names.includes(name));
+  private existsDockerContainer(): boolean {
+    const listContainerCommand = ` docker container ls --all --quiet --filter "name=${this.containerName}"`;
+    const response = cmd(listContainerCommand);
+    return response.stdout.trim() !== "";
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,29 @@
 import { spawnSync } from "node:child_process";
 import { CmdResponse } from "./types";
+import { Store } from "./Store";
 
-export function getChunks(stream: NodeJS.ReadableStream): Promise<Buffer> {
-  const chunks: Uint8Array[] = [];
-  return new Promise((resolve, reject) => {
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("end", () => resolve(Buffer.concat(chunks)));
-    stream.on("error", (err) => reject(err));
-  });
+/**
+ * Checks if there is an error from running a command, and if there is, adds a message
+ * to the store with the error
+ * @param response the response from running the command
+ * @param dockerCmdOutput the output of the docker command
+ */
+export function checkCmdError(response: CmdResponse, dockerCmdOutput: string): void {
+  if (response.stderr) {
+    Store.getInstance().addMessage(response.stderr.split("\n").join("<br>"));
+  }
 }
 
+/**
+ * Runs a command and logs the output to the store
+ * @param command the command to run
+ * @returns the response from running the command
+ */
 export function cmd(command: string): CmdResponse {
+  Store.getInstance().addMessage(`Running command: ${command}`);
   return spawnSync(command,{
     shell: true,
-    stdio: "inherit",
+    stdio: "pipe",
     cwd: process.cwd(),
     encoding: "utf-8",
     env: process.env
